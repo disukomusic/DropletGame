687b7f4bd984188847907747b00a39347872309d
Disuko <disukomusic@gmail.com>
2023-02-01 01:14:53 -0500

    Added player model and adjusted camera

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -2,22 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RollPlayer : MonoBehaviour
+public class PlayerMove : MonoBehaviour
 {
     private Rigidbody _rigidbody;
     public float movementSpeed;
     
     void Start()
     {
+        
         _rigidbody = GetComponent<Rigidbody>();
     }
     
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
 
-        Vector3 direction = new Vector3(x: horizontal, y:0, z: vertical);
+        Vector3 direction = new Vector3(x: horizontal, y:0, z: -vertical);
         _rigidbody.AddForce(direction * movementSpeed);
     }
 }
