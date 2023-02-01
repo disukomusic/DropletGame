aa753e9b326dd950fcbab33f6bc5e6f71de44a31
Disuko <disukomusic@gmail.com>
2023-02-01 15:44:37 -0500

    Revert "Merge branch 'main' of https://github.com/disukomusic/DropletGame"
    This reverts commit c89d4667a0d573ac3dbf12349dfdd076428a135c, reversing
    changes made to 11b10a2df7d6a044d87ae634a8f83da7f06d6b3c.

--- Assets/OToon- URP Toon Shading/Shaders/Library/ShadergraphFunction.hlsl
+++
@@ -1,9 +0,0 @@
-void ObjectNormalToEye_half(half3 normalOS, out half3 Out)
-{
-    Out = normalize(mul((float3x3)UNITY_MATRIX_IT_MV, normalOS));
-}
-
-void ObjectNormalToEye_float(half3 normalOS, out half3 Out)
-{
-    Out = normalize(mul((float3x3)UNITY_MATRIX_IT_MV, normalOS));
-}
