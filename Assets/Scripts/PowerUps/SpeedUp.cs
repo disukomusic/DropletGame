a956b4b23d9081a1238f958fa50d1e71c9ad3ce7
stephr22 <stephanie.rios@chatham.edu>
2023-02-04 13:51:25 -0500

    SpeedUp

--- Assets/Scripts/PowerUps/SpeedUp.cs
+++ Assets/Scripts/PowerUps/SpeedUp.cs
@@ -6,12 +6,16 @@ using UnityEngine;
 public class SpeedUp : PowerUp
 {
     public Player _player;
-
+    public Player movementSpeed;
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            //_player
+            Clear();
+            PowerUpSpawner.count -= 1;
+            movementSpeed + 10;
+            Destroy(gameObject);
         }
     }
 }
