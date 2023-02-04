f73c8125c7f7d24ccbc844e89c90fb648e1f702d
Disuko <disukomusic@gmail.com>
2023-02-03 19:43:43 -0500

    commented smooth rotation code

--- Assets/Scripts/Player/PlayerRotate.cs
+++ Assets/Scripts/Player/PlayerRotate.cs
@@ -19,10 +19,14 @@ public class PlayerRotate : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(-horizontalInput, 0, -verticalInput).normalized;
+        
+        //if the movement is (0,0,0) [no input from player], exit the method for this frame
         if (movement == Vector3.zero)
         {
             return;
         }
+        
+        //Rotate towards the target rotation
         Quaternion targetRotation = Quaternion.LookRotation(movement);
         targetRotation = Quaternion.RotateTowards(transform.rotation, targetRotation, 360 * Time.deltaTime);
         _transform.rotation = targetRotation;
